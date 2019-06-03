c9917734b6fda0b34717fa75b34deadc7138fcf9
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-03 23:38:53 +0100

    Fixed import statement and added docstrings
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Cards/Deck_class.py
+++ Cards/Deck_class.py
@@ -1,8 +1,11 @@
-import card
+from Cards.Card_class import *
 
 
 class Deck:
     def __init__(self):
+        """
+        This creates a standard deck of cards
+        """
         self.deck = []
         for suit in ['s', 'c', 'h', 'd']:
             for value in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']:
