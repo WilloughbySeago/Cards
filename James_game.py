e87c1e620d072c05ef70676b08bd738605c566ec
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-04 01:21:07 +0100

    Created round2 function
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- James_game.py
+++ James_game.py
@@ -1,4 +1,5 @@
 from Cards.Deck_class import *
+import string
 
 
 def round1(cards, guess):
@@ -6,10 +7,37 @@ def round1(cards, guess):
     This function draws a card and then compares the colour to the guess
     :param cards: Deck
     :param guess: 'red' or 'black'
-    :return: bool
+    :return: bool, Card
     """
     card = cards.deck.pop(0)
     if card.colour == guess:
-        return True
+        return True, card
     else:
-        return False
+        return False, card
+
+
+def round2(cards, guess, previous_card):
+    """
+    This function draws a card and then compares it to the guess of higher or lower
+    :param cards: Deck
+    :param guess: 'higher' or 'lower'
+    :return: bool, Card
+    """
+    card = cards.deck.pop(0)
+    if card.value in string.digits or card.value == '10':
+        card_val = int(card.value)
+    elif card.value == 'j':
+        card_val = 11
+    elif card.value == 'q':
+        card_val = 12
+    elif card.value == 'k':
+        card_val = 13
+    else:
+        card_val = 14
+    if guess == 'higher' and card_val > previous_card.value:
+        return True, card
+    elif guess == 'lower' and card_val < previous_card.value:
+        return True, card
+    elif card_val == previous_card.value:
+        return round2(cards, guess, previous_card)
+
