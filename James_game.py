6fe10f8cb0708e1517aea6d51578b1b5ad9ae92b
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-04 01:36:09 +0100

    Added str_to_int function to get an integer representation of the value of the card instead of a string
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- James_game.py
+++ James_game.py
@@ -1,5 +1,6 @@
 from Cards.Deck_class import *
 import string
+import random
 
 
 def round1(cards, guess):
@@ -21,23 +22,32 @@ def round2(cards, guess, previous_card):
     This function draws a card and then compares it to the guess of higher or lower
     :param cards: Deck
     :param guess: 'higher' or 'lower'
+    :param previous_card: Card
     :return: bool, Card
     """
     card = cards.deck.pop(0)
-    if card.value in string.digits or card.value == '10':
-        card_val = int(card.value)
-    elif card.value == 'j':
-        card_val = 11
-    elif card.value == 'q':
-        card_val = 12
-    elif card.value == 'k':
-        card_val = 13
-    else:
-        card_val = 14
-    if guess == 'higher' and card_val > previous_card.value:
+    card_val = str_to_int(card)
+    prev_val = str_to_int(previous_card)
+    if guess == 'higher' and card_val > prev_val:
         return True, card
-    elif guess == 'lower' and card_val < previous_card.value:
+    elif guess == 'lower' and card_val < prev_val:
         return True, card
-    elif card_val == previous_card.value:
+    elif card_val == prev_val:
+        print('same card')
         return round2(cards, guess, previous_card)
+
+
+def str_to_int(card):
+    val = card.value
+    if val in string.digits or val == '10':
+        val = int(val)
+    elif val == 'j':
+        val = 11
+    elif val == 'q':
+        val = 12
+    elif val == 'k':
+        val = 13
+    else:
+        val = 14
+    return val
 
