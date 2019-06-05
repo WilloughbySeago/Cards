8cfb42b9736ab4352825f84aa904e00ab91379c8
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-05 13:04:37 +0100

    Added plot of histogram
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Cards/Red_or_black_analysis.py
+++ Cards/Red_or_black_analysis.py
@@ -13,12 +13,26 @@ with open('results.csv', mode='r') as results:
         result_array.append(row[0])
 
 result_array = np.array(result_array, dtype=int)
-print(result_array)
-print(type(result_array))
-print(result_array.dtype)
+# print(result_array)
 average = int(round(np.mean(result_array)))
-print(average)
+print(f'Mean = {average}')
 std_dev = np.std(result_array)
-print(std_dev)
+print(f'Standard deviation = {std_dev}')
 
-
+min_result = np.amin(result_array)
+max_result = np.amax(result_array)
+range_result = max_result - min_result
+print(min_result, max_result, range_result)
+resolution = 100
+padding = 20
+min_bin = int(np.ceil(min_result/resolution)*resolution) - padding
+max_bin = int(np.ceil(max_result/resolution)*resolution) + padding
+print(min_bin, max_bin)
+bins = [i for i in range(min_bin, max_bin, resolution)]
+print(bins)
+plt.hist(result_array, bins=bins)
+plt.title(f'A histogram of the results of red vs. black with the strategy of picking the opposite of the first card'
+          + f'class width = {resolution}')
+plt.xlabel('Correct guesses out of 10^6')
+plt.ylabel('Frequency')
+plt.show()
