da489abe8f7c6a79505810b8ed1ddabe37cd6ab6
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-05 12:47:08 +0100

    Input from csv and calculate mean/std. deviation
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Cards/Red_or_black_analysis.py
+++ Cards/Red_or_black_analysis.py
@@ -2,4 +2,23 @@
 This is a program to analyse the results from Red_or_black
 """
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+result_array = []
+with open('results.csv', mode='r') as results:
+    results_reader = csv.reader(results)
+    next(results_reader)
+    for row in results_reader:
+        result_array.append(row[0])
+
+result_array = np.array(result_array, dtype=int)
+print(result_array)
+print(type(result_array))
+print(result_array.dtype)
+average = int(round(np.mean(result_array)))
+print(average)
+std_dev = np.std(result_array)
+print(std_dev)
+
 
