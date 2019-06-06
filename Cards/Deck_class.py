41e2fac834feb449c4f83507547c674b9ed5441e
WilloughbySeago <willoughbyseago123@hotmail.com>
2019-06-06 01:35:06 +0100

    Commenting
    Signed-off-by: WilloughbySeago <willoughbyseago123@hotmail.com>

--- Cards/Deck_class.py
+++ Cards/Deck_class.py
@@ -9,7 +9,9 @@ class Deck:
         """
         This creates a standard deck of cards
         """
-        self.deck = []
+        self.deck = []  # deck is just an array of Card objects
+        # create all 13 cards of each suit
         for suit in ['s', 'c', 'h', 'd']:
+            # for each suit create each card for every possible value
             for value in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']:
                 self.deck.append(Card(value, suit))
